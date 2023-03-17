d72ab14a32b342bcfbb73a8846407a028cb0f457
Ekaterina Gorbacheva <katerina.smr@gmail.com>
2023-03-17 23:40:17 +0300

    updates after feedback

--- hw2/src/snilsValidator/SnilsValidatorImpl.java
+++ hw2/src/snilsValidator/SnilsValidatorImpl.java
@@ -3,19 +3,20 @@ package snilsValidator;
 public class SnilsValidatorImpl implements SnilsValidator {
     @Override
     public boolean validate(String snils) {
-        boolean result = false;
-        if (snils.matches("\\d{11}")) {
-            char[] chars = snils.toCharArray();
-            int sum = 0;
-            for (int i = 0; i < 9; i++) {
-                int x = Character.digit(chars[i], 10);
-                sum += x * (9 - i);
-            }
-            //условие для вычисления контрольной суммы по сути сводится к вычислению остатка от деления на 101
-            int remaining = sum % 101;
-            int controlSum = remaining == 100 ? 0 : remaining;
-            result = controlSum == Integer.parseInt(snils.substring(snils.length()-2));
+        if (!snils.matches("\\d{11}")) {
+            return false;
         }
-        return result;
+
+
+        char[] chars = snils.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int x = Character.digit(chars[i], 10);
+            sum += x * (9 - i);
+        }
+        //условие для вычисления контрольной суммы по сути сводится к вычислению остатка от деления на 101
+        int remaining = sum % 101;
+        int controlSum = remaining == 100 ? 0 : remaining;
+        return controlSum == Integer.parseInt(snils.substring(snils.length() - 2));
     }
 }

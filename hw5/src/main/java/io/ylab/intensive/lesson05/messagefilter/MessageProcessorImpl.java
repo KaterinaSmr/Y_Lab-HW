2ef895a9fcee9a690d3862fbc2fe140cdf2165e4
Ekaterina Gorbacheva <katerina.smr@gmail.com>
2023-04-04 15:49:57 +0300

    hw5 finished

--- hw5/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageProcessorImpl.java
+++ hw5/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageProcessorImpl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class is responsible for masking curse words in incoming messages. Message is received from RabbitMQ queue.
@@ -12,6 +14,7 @@ import java.sql.SQLException;
  * The result message is sent to RabbitMQ queue.</br>
  * A word is identified as a curse word if {@link DbClient#isBadWord(String)} returns true.
  * {@link RabbitClient} is used for communicating with RabbitMQ.
+ *
  * @see RabbitClient
  * @see DbClient
  */
@@ -19,6 +22,7 @@ import java.sql.SQLException;
 public class MessageProcessorImpl implements MessageProcessor {
   private final RabbitClient rabbitClient;
   private final DbClient dbClient;
+  private final String wordBorder = "([\\s.,;:!?]|^|$)";
 
   public MessageProcessorImpl(RabbitClient rabbitClient, DbClient dbClient) {
     this.rabbitClient = rabbitClient;
@@ -27,6 +31,7 @@ public class MessageProcessorImpl implements MessageProcessor {
 
   /**
    * Gets single message from queue, performs curse words masking and sends updated message to the queue.
+   *
    * @throws IOException
    * @throws SQLException
    */
@@ -36,23 +41,47 @@ public class MessageProcessorImpl implements MessageProcessor {
       return;
     }
 
-    String[] words = message.split("[\\s\\n\\r .,;:!?]");
+    String[] words = message.split(wordBorder);
+    StringBuilder result = new StringBuilder(message);
     for (String s : words) {
       if (dbClient.isBadWord(s)) {
-        message = message.replaceFirst(s, replacementWord(s));
+        replaceBadWord(result, s);
       }
     }
 
-    rabbitClient.publish(message);
+    rabbitClient.publish(result.toString());
+  }
+
+  /**
+   * Replaces specific curse word in a message to a masked version.
+   * First, the curse word is found withing the message itself, taking into account valid word limiting characters,
+   * which is one of {@link MessageProcessorImpl#wordBorder} characters.
+   * Next, withing this match the curse word itself is searched. Based on these 2 searches, the start position of the
+   * word to be replaced is determined. The {@link MessageProcessorImpl#getReplacementWord(String)} provides the masked
+   * version of the curse word.
+   * @param sb message, in which the curse word should be masked
+   * @param badWord word to be masked
+   */
+  private void replaceBadWord(StringBuilder sb, String badWord) {
+    Matcher matcher = Pattern.compile(wordBorder + badWord + wordBorder).matcher(sb);
+    if (matcher.find()) {
+      String match = matcher.group(0);
+      Matcher matcherInternal = Pattern.compile(badWord).matcher(match);
+      if (matcherInternal.find()) {
+        int startPos = matcher.start() + matcherInternal.start();
+        sb.replace(startPos, startPos + badWord.length(), getReplacementWord(badWord));
+      }
+    }
   }
 
   /**
    * Masks the string specified, so that only first and last symbols are the same and all other symbols are replaced
    * with '*'. The length of the string remains the same.
+   *
    * @param word curse word to be masked
    * @return masked version of the string specified
    */
-  private String replacementWord(String word) {
+  private String getReplacementWord(String word) {
     if (word.length() <= 2) {
       return word;
     }
